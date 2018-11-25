a7a53f7488db227b36f5ce645a9ff97f6934d94d
Usuario <Usuario@192.168.85.195>
2018-11-25 12:50:41 +0100

    Diversas mejoras. - Muestra nombres de las tablas - Muestra los nombres de los campos de la tabla seleccionada. - Sólo permite ejecutar sentencias de no acción. - Traducciones etiquetas.

--- Controller/SqlEditor.php
+++ Controller/SqlEditor.php
@@ -14,13 +14,14 @@ class SqlEditor extends \FacturaScripts\Core\Base\Controller
 
     public $query;
     public $results;
+    public $columnsTable;
 
     /**
      * Return the basic data for this page.
      *
      * @return array
      */
-    public function getPageData()
+    public function getPageData(): Array
     {
         $pageData = parent::getPageData();
         $pageData['title'] = 'sql Editor';
@@ -28,6 +29,29 @@ class SqlEditor extends \FacturaScripts\Core\Base\Controller
         $pageData[icon] = 'fas fa-database';
 
         return $pageData;
+    }
+
+    /**
+     * Execute action
+     *
+     * @param string $action
+     * @return boolean
+     */
+    private function executeAction($action)
+    {
+        switch ($action) {
+            case 'getFields':
+                $tableName = $this->request->get('table');
+                if ($tableName !== '') {
+                    $this->columnsTable = $this->getColumns($tableName);
+                    $this->setTemplate(false);
+                    $this->response->setContent(json_encode(array_keys($this->columnsTable)));
+                }
+                break;
+            default:
+                return FALSE;
+                break;
+        }
     }
 
     /**
@@ -45,32 +69,46 @@ class SqlEditor extends \FacturaScripts\Core\Base\Controller
         $query = $this->request->get('query', '');
 
         if ($query !== '') {
-            $this->query = $query;
-            if ($this->validQuery()) {
-                $this->execQuery();
+            if ($this->user->admin) {
+                $this->query = $query;
+
+                if ($this->validQuery()) {
+                    $this->execQuery();
+                } else {
+                    $this->miniLog->alert($this->i18n->trans('invalid-sentence'));
+                    $this->results = [];
+                }
             } else {
-                $this->miniLog->alert('invalid-sentence');
+                $this->miniLog->aler('only-admin');
             }
+        }
+        $action = $this->request->request->get('action');
+        if ($action) {
+            $this->executeAction($action);
         }
     }
 
     /**
      * Return true is is authorized query
+     *
+     * @return boolean
      */
     private function validQuery()
     {
+
         $validSentences = ['select', 'show', 'describe', 'explain'];
         $invalidSentences = ['drop', 'delete', 'insert', 'update'];
 
-
         foreach ($invalidSentences as $sentence) {
-            if (strpos($this->query, $sentence) > 0)
+            if (strpos($this->query, $sentence) !== FALSE) {
+
                 return false;
+            }
         }
 
         foreach ($validSentences as $sentence) {
 
-            if (strpos($this->query, $sentence) >= 0) {
+            if (strpos($this->query, $sentence) !== FALSE) {
                 return true;
             }
         }
@@ -80,6 +118,8 @@ class SqlEditor extends \FacturaScripts\Core\Base\Controller
     /**
      * Returns array width result values of exec query
      * @param string $query
+     *
+     *
      */
     private function execQuery()
     {
@@ -89,7 +129,7 @@ class SqlEditor extends \FacturaScripts\Core\Base\Controller
         if ($data) {
             $this->results = $data;
         } else {
-            $this->miniLog->notice('without-results');
+            $this->miniLog->notice($this->i18n->trans('without-results'));
         }
     }
 
@@ -97,8 +137,34 @@ class SqlEditor extends \FacturaScripts\Core\Base\Controller
      * Returns array width columns title of results
      * @return Array
      */
-    public function columnsTitle()
+    public function columnsTitle(): Array
     {
         return empty($this->results) ? [] : array_keys($this->results[0]);
+    }
+
+    /**
+     * Return Tablenames of database
+     * @return Array
+     */
+    public function getTables(): Array
+    {
+
+        $dataBase = new \FacturaScripts\Core\Base\DataBase();
+        return $dataBase->getTables();
+    }
+
+    /**
+     * Return column names of table
+     * @param string $tableName
+     * @return Array
+     */
+    public function getColumns($tableName = ''): Array
+    {
+        if ($tablename !== '') {
+            $dataBase = new \FacturaScripts\Core\Base\DataBase();
+            return $dataBase->getColumns($tableName);
+        } else {
+            return [];
+        }
     }
 }
